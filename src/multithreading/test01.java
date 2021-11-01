5d45d7e0baf714a70b7de52eb487a3b52027fde1
in3des <62700347+in3des@users.noreply.github.com>
2021-11-01 19:41:37 +0400

    thread3-4

--- src/multithreading/test01.java
+++ src/multithreading/test01.java
@@ -4,6 +4,10 @@ public class test01 {
 
     Thread thread = new Thread();
     Thread thread1 = new Thread();
+    Thread thread3 = new Thread();
+    Thread thread4 = new Thread();
+
+
 
     String s = "Git МОЩЬ!";
 
