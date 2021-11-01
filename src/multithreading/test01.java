a5dbbeba08a4a21cf12c89389abc18033f3ac584
Roman <ghostrog@mail.ru>
2021-11-01 20:24:12 +0400

    String

--- src/multithreading/test01.java
+++ src/multithreading/test01.java
@@ -1,16 +1,22 @@
 package multithreading;
 
+import java.lang.management.BufferPoolMXBean;
+
 public class test01 {
 
     Thread thread = new Thread();
     Thread thread1 = new Thread();
+    Thread thread3 = new Thread();
+    Thread thread4 = new Thread();
+
 
 
     static String s = "Git МОЩЬ!";
 
     public static void main(String[] args) {
         s = "Git неmoshb";
-        BufferPoolMXBean bean;
+
+        String a = "asd";
     }
 
 }
